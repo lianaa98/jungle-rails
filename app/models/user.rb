aee78072b9eda3d382c043d8ccd9d404b07f34c2
lianaa98 <liana.kan98@gmail.com>
2023-06-06 13:05:20 -0700

    authenticate with credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  # authenticate_with_credentials
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+  
 end
