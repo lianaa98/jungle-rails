d91f12688974377237ec340ed9b57b9aa8b2b0b4
lianaa98 <liana.kan98@gmail.com>
2023-06-05 17:07:25 -0700

    changed name to lastname and firstname in table user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
